55da939d0b14480627ece995f830ab498e4ff8c5
Kimjh1214 <hzpotter1214@naver.com>
2021-05-03 18:24:41 +0900

    dd

--- 04company/js/script.js
+++ 04company/js/script.js
@@ -2,4 +2,18 @@ $(document).ready(function(){
   $(".slider").bxSlider({
     pager:false
   });
+  const headerT = $("#headerTop").offset().top;
+  $(window).scroll(function(){
+    let scrollY = $(window).scrollTop();
+
+    console.log(headerT);
+    if( headerT < scrollY){
+      $("#headerTop").addClass("fixed");
+
+    }else{
+      $("#headerTop").removeClass("fixed");
+    
+    }
+  });
+
 });
