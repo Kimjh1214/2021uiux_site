cae7b6416d3eabccc08089b4bbfd6f2fd1aeb742
Kimjh1214 <hzpotter1214@naver.com>
2021-05-12 18:22:09 +0900

    20210512

--- 04company/js/script.js
+++ 04company/js/script.js
@@ -14,5 +14,20 @@ $(document).ready(function(){
 
     }
   });
+  $(".toggleMenu").click(function(){
+    const toggle = $(".toggleMenu").attr("class"); //get
 
+    if(toggle == "toggleMenu"){
+      $(".toggleMenu").addClass("change");
+      $("#main_menu").stop().animate({top:0});
+    }else{
+      $(".toggleMenu").removeClass("change");
+      $("#main_menu").stop().animate({top:"-100%"});
+    }
+  });
+
+  $(window).resize(function(){
+    $("#main_menu").removeAttr("style");
+    $(".toggleMenu").removeClass("change");
+  });
 });
